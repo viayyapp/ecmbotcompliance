d5a25953f7fd9548c73d921511423ebe90fc96a5
Vignesh Ayyappan <viayyapp@in.ibm.com>
2017-04-24 19:08:35 +0530

    Fixed Code issues

--- tone_analyze.py
+++ tone_analyze.py
@@ -6,7 +6,7 @@ def tone_analyze_fun():
 	tone_analyzer1 = ToneAnalyzerV3(
 
 		username= '976ebca7-68c3-4ca2-abcc-d0d80d79419c',
-		password= 'dTi3SDLcPfwe'
+		password= 'dTi3SDLcPfwe',
 		# username = '20c2903e-48a9-4fd5-8f0b-5e699fa5343e',
 		# password = 'ZC2WBeLbXUXs',
 		version = '2016-05-19')
