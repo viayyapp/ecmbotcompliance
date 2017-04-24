7aeda8143f855dced4b9754dfc1dc105de331ab9
Vignesh Ayyappan <viayyapp@in.ibm.com>
2017-04-24 19:45:56 +0530

    Changes in credentials

--- conversation.py
+++ conversation.py
@@ -4,8 +4,8 @@ from json import JSONEncoder
 def conversation_fun():
 	
 	conversation = ConversationV1(
-	username='18c651c1-c2f4-403a-88bd-a920bd753c88',
-	password='42HMzjtHrvCf',
+	username='8f852f62-ded3-4c89-b696-f6999670f391',
+	password='wMCxakn17KSZ',
 	version='2017-04-24')
 
 	return conversation
