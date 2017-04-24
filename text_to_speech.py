315aaf5bc0de0b71fb92d51d7031f952f3f4eb35
Vignesh Ayyappan <viayyapp@in.ibm.com>
2017-04-24 19:00:31 +0530

    Changed Credentials

--- text_to_speech.py
+++ text_to_speech.py
@@ -3,8 +3,10 @@ from watson_developer_cloud import TextToSpeechV1
 def text_to_speech_fun():
 
 	text_to_speech = TextToSpeechV1(
-	username='8921eb13-5eb4-4286-9add-5a7870ba4ecf',
-	password='tKJBMhuw42nL',
+		username= 'f4f78f13-490f-4ea4-b7a8-d5ebf470011d',
+		password= 'pr0IqFp70Dyl'
+	# username='8921eb13-5eb4-4286-9add-5a7870ba4ecf',
+	# password='tKJBMhuw42nL',
 	x_watson_learning_opt_out=True)
-    
+
 	return text_to_speech
--- tone_analyze.py
+++ tone_analyze.py
@@ -4,8 +4,11 @@ from watson_developer_cloud import ToneAnalyzerV3
 def tone_analyze_fun():
 
 	tone_analyzer1 = ToneAnalyzerV3(
-	username = '20c2903e-48a9-4fd5-8f0b-5e699fa5343e',
-	password = 'ZC2WBeLbXUXs',
-	version = '2016-05-19')
-    
+
+		username= '976ebca7-68c3-4ca2-abcc-d0d80d79419c',
+		password= 'dTi3SDLcPfwe'
+		# username = '20c2903e-48a9-4fd5-8f0b-5e699fa5343e',
+		# password = 'ZC2WBeLbXUXs',
+		version = '2016-05-19')
+
 	return tone_analyzer1
